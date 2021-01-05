9e51a38c46e24020994c480d144b5c7cbd3867a0
José Gomes <zegomes@gmail.com>
2021-01-05 22:25:05 +0000

    Added header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+        <p className="footer-text">
+            Made by <a href="https://zegomes.net">José Gomes</a> <a href="https://github.com/ze-gomes"><img className="inline align-middle"
+                    src="https://raw.githubusercontent.com/ze-gomes/weather-app/main/images/github-white.svg" alt=""></img></a>
+        </p>
+    </div>
+    )
+}
+
+export default Footer
