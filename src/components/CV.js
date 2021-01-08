d90e23c5fc56096d40173932081b2cc09140b79b
José Gomes <zegomes@gmail.com>
2021-01-08 19:51:19 +0000

    Added main CV class

---
+++ src/components/CV.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CV extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CV
--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {ImLocation} from "react-icons/im";
 import {AiFillPhone} from "react-icons/ai";
 
 
